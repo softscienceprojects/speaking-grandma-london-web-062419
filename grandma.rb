54b356c54e9b943109089281104f53a12c6f7935
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-06 10:58:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if
+  if phrase.upcase
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-  elsif condition
 
 # If you shout, she can hear you (or at least she thinks so)
 # and yells back
