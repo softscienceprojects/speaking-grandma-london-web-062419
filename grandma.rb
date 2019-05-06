f9150e840f0356a67a3d6e95d39380125cee8e7e
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-06 11:13:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
- elsif phrase != "I LOVE YOU GRANDMA!" && phrase.upcase == false
+ elsif phrase != "I LOVE YOU GRANDMA!" && phrase.upcase == true
     return "HUH?! SPEAK UP, SONNY!"
  else
    return "NO, NOT SINCE 1938!"
